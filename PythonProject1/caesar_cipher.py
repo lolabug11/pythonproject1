005015500618b7d4c2f77fcf616ff045c7a126d2
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2025-08-21 06:08:39 +0000

    added caesar_cipher to main and added decryption

--- PythonProject1/caesar_cipher.py
+++ PythonProject1/caesar_cipher.py
@@ -14,13 +14,29 @@ def encrypt(text, shift):
         else:
             encrypted_text += char
     return encrypted_text
-def decrypt(text, key):
-    pass
-text = input('What is your plaintext/Ciphertext\n')
-key = input('What is the key to your cypher?\n')
-encrypt_or_decrypt = input('Do you want to encrypy or decrypt your text (decrypt auto swaps the key)?\n')
-if 'encrypt' in encrypt_or_decrypt.lower():
-    print(encrypt(text, key))
-else:
-    key = key * -1
-    print(decrypt(text, key))
+def decrypt(text, shift):
+    text_as_list = []
+    decrypted_text = ''
+    for char in text:
+        text_as_list.append(char)
+    print(text_as_list)
+    for char in text_as_list:
+        if char.isalpha():
+            letter_key = alphabet.index(char)
+            new_letter = (letter_key + shift) % 26
+            new_letter = alphabet[new_letter]
+            decrypted_text += new_letter
+        else:
+            decrypted_text += char
+    return decrypted_text
+def caesar_cypher():
+    text = input('What is your plaintext/Ciphertext\n')
+    key = int(input('What is the key to your cypher?\n'))
+    encrypt_or_decrypt = input('Do you want to encrypt or decrypt your text (decrypt auto swaps the key)?\n')
+    if 'encrypt' in encrypt_or_decrypt.lower():
+        text = text.lower()
+        print(encrypt(text, key))
+    else:
+        text = text.lower()
+        key = key * - 1
+        print(decrypt(text, key))
--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -4,9 +4,11 @@ from caesar_cipher import *
 if __name__ == "__main__":
     repeat = True
     while repeat == True:
-        miny_project_select = input('What project do you want to use?\n1. FizzBuzz\n2. palindrome checker\n')
+        miny_project_select = input('What project do you want to use?\n1. FizzBuzz\n2. palindrome checker\n3. caesar cypher\n')
         if 'fizz' in miny_project_select.lower() or 'buz' in miny_project_select.lower() or '1' in miny_project_select.lower():
             fizz_buzz()
         if 'palindrome' in miny_project_select.lower() or 'checker' in miny_project_select.lower() or '2' in miny_project_select.lower():
             palindrome_checker()
+        if 'caeser' in miny_project_select.lower() or 'cypher' in miny_project_select.lower() or '3' in miny_project_select.lower():
+            caesar_cypher()
         repeat = bool(input('Do you want to repeat (True or False, this is case sensitive)?\n'))
