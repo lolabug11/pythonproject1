99eeb681ffca2cd3f06818c08fac6e4906dc8224
Lola Bug <wambolt.w5756@gmail.com>
2025-08-21 16:17:25 -0400

    updated main.py for the word counter

--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -3,8 +3,8 @@ from palindrone_checker import *
 from caesar_cipher import *
 from word_frequincy_counter import *
 if __name__ == "__main__":
-    repeat = True
-    while repeat:
+    repeat = 'y'
+    while 'y' in repeat:
         miny_project_select = input('What project do you want to use?\n1. FizzBuzz\n2. palindrome checker\n3. caesar cypher\n')
         if 'fizz' in miny_project_select.lower() or 'buz' in miny_project_select.lower() or '1' in miny_project_select.lower():
             fizz_buzz()
@@ -14,4 +14,4 @@ if __name__ == "__main__":
             caesar_cypher()
         if 'word' in miny_project_select.lower() or "frequency" in miny_project_select.lower() or 'counter' in miny_project_select.lower() or '4' in miny_project_select.lower():
             word_frequincy_counter()
-        repeat = bool(input('Do you want to repeat (True or False, this is case sensitive)?\n'))
+        repeat = input('Do you want to repeat (yes or no)?\n')
